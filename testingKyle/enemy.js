1c7410b244eacb600f8ee5caa27164a051563b3d
Kyle Burke <kyle.n.burke@gmail.com>
2017-11-24 18:13:27 -0500

    Remove off screen enemies (#3)

--- testingKyle/enemy.js
+++ testingKyle/enemy.js
@@ -1,17 +1,17 @@
 function Enemy(floorPosY, canvasWidth) {
 	var color = "red";
-	var radius = 25;
-	var centerX = 800;
-	var centerY = floorPosY - radius - 10;
+	this.radius = 25;
+	this.centerX = 800;
+	this.centerY = floorPosY - this.radius - 10;
 	var speed = 5;
 
 	this.update = function() {
-		centerX -= 5;
+		this.centerX -= 5;
 	}
 
 	this.draw = function(canvas) {
 		canvas.beginPath();
-		canvas.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+		canvas.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI, false);
 		canvas.fillStyle = color;
 		canvas.fill();
 		canvas.lineWidth = 5;
