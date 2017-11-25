67a6505a5c86b299be8483d4c9c71751e406f5de
Ryan Everett <ryaneverett33@outlook.com>
2017-11-25 00:23:10 -0500

    debugging with no console

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-
+	var message = "Request: " + request + "\n";
+	message += "Cwd: " + process.cwd() + "\n";
+	message += "HELLO BITCHES" + "\n";
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end(message);
 
 });
 
