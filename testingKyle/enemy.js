7df0c166eb05786a98d96f9b8c535d0da79e1cf3
KyleNBurke <kyle.n.burke@gmail.com>
2017-11-26 16:55:07 -0500

    Implemented collision detection and menu screens

--- testingKyle/enemy.js
+++ testingKyle/enemy.js
@@ -1,17 +1,17 @@
-function Enemy(floorPosY, canvasWidth, type) {
+function Enemy(floorPosY, canvasWidth, type) { //type: floor, ariel
 	var color = type == "floor" ? "red" : "green";
 	this.width = 50;
-	var height = 50;
+	this.height = 50;
 	this.posX = canvasWidth;
-	var posY = type == "floor" ? floorPosY - height : floorPosY - height - 70;
+	this.posY = type == "floor" ? floorPosY - this.height : floorPosY - this.height - 70;
 	var speed = 5;
 
 	this.update = function() {
-		this.posX -= 5;
+		this.posX -= speed;
 	}
 
-	this.draw = function(canvas) {
+	this.draw = function() {
 		canvas.fillStyle = color;
-		canvas.fillRect(this.posX, posY, this.width, height);
+		canvas.fillRect(this.posX, this.posY, this.width, this.height);
 	}
 }
