281419073a34ee1eee8d1f8e6779b077df2dccc1
Ryan Everett <ryaneverett33@outlook.com>
2017-12-08 00:57:58 -0500

    Fix User Set

--- database/setUser.js
+++ database/setUser.js
@@ -35,13 +35,22 @@ function set(column, newvalue, name, callback) {
             return;
           }
         }
+        else {
+          console.log("Set succeeded");
+          //user["name"] = name;
+          if (callback != null) {
+            pool.release(connection);
+            callback(1);
+            return;
+          }
+        }
       });
       request.addParameter('Value', TYPES.NVarChar, newvalue);
       request.addParameter('Name', TYPES.NVarChar, name);
     }
 
     else if (column === 'Wins') {
-      request = new Request("UPDATE users SET Wins = @Value WHERE Name = @Name;", function (err) {
+      request = new Request("UPDATE users SET Wins = @Value WHERE Name = @Name;", function (err, rowcount) {
         if (err) {
           console.log(err);
           connection.release();
@@ -53,6 +62,15 @@ function set(column, newvalue, name, callback) {
           if (callback != null) {
             pool.release(connection);
             callback(null);
+            return;
+          }
+        }
+        else {
+          console.log("Set succeeded");
+          //user["name"] = name;
+          if (callback != null) {
+            pool.release(connection);
+            callback(1);
             return;
           }
         }
