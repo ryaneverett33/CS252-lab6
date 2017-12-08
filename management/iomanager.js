62e0b0c89f26f0accfde6198a4c898446e4474e9
Ryan Everett <ryaneverett33@outlook.com>
2017-12-07 23:49:04 -0500

    Handle leaving a match on disconnect, safely emit messages

--- management/iomanager.js
+++ management/iomanager.js
@@ -15,6 +15,13 @@ var currentGameCount = 0;
 //game settings
 var MAXPLAYERS = 2;
 
+//Safely emits a message over a socket, no err today bois
+this.safeEmit = function(socket, type, message) {
+    if (socket.connected) {
+        socket.emit(type, message);
+    }
+}
+
 //FROM MDN
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
@@ -108,7 +115,7 @@ function findOpenRoom(roomid) {
 // Match.foundGame : {error : string} | {roomid : int}
 function findGame(gameObj, socket) {
     if (gameObj.username == null) {
-        socket.emit('Match.error', {
+        safeEmit(socket, 'Match.error', {
             error : "Invalid Arguments"
         });
     }
@@ -117,7 +124,7 @@ function findGame(gameObj, socket) {
     if (match == null) {
         console.log("Did not find open room");
         //couldn't find a match
-        socket.emit('Match.error', {
+        safeEmit(socket, 'Match.error', {
             error : "Couldn't find match with given roomid"
         });
         return;
@@ -127,7 +134,7 @@ function findGame(gameObj, socket) {
         //join game
         if (match.join(gameObj.username, socket)) {
             console.log("joined match");
-            socket.emit('Match.foundGame', {
+            safeEmit(socket, 'Match.foundGame', {
                 roomid : match.roomid
             });
             if (match.isFull()) {
@@ -138,7 +145,7 @@ function findGame(gameObj, socket) {
         }
         else {
             console.log("Failed to join match");
-            socket.emit('Match.error', {
+            safeEmit(socket, 'Match.error', {
                 error : "Failed to join match"
             });
             return;
@@ -165,13 +172,13 @@ function handlePlayerAction(socket, actionObj) {
     console.log(actionObj);
     if (actionObj.roomid == null) {
         console.log("HandlePlayerAction was given an invalid object, roomid is null");
-        socket.emit("Player.error", { error : "Roomid is invalid"});
+        safeEmit(socket, "Player.error", { error : "Roomid is invalid"});
         return;
     }
     var match = getRoom(actionObj.roomid);
     if (match == null) {
         console.log("HandlePlayerAction couldn't get match");
-        socket.emit("Player.error", { error : "Roomid is invalid"});
+        safeEmit(socket, "Player.error", { error : "Roomid is invalid"});
         return;
     }
     match.handlePlayerAction(socket, actionObj);
@@ -183,12 +190,12 @@ function handlePlayerAction(socket, actionObj) {
 function getBoard(socket, boardObj) {
     console.log("Recieved: " + boardObj);
     if (boardObj.roomid == null) {
-        socket.emit("Match.error", { error : "Roomid is invalid"});
+        safeEmit(socket, "Match.error", { error : "Roomid is invalid"});
         return;
     }
     var match = getRoom(boardObj.roomid);
     if (match == null) {
-        socket.emit("Match.error", { error : "Roomid is invalid"});
+        safeEmit(socket, "Match.error", { error : "Roomid is invalid"});
         return;
     }
     match.getBoard(socket, boardObj);
@@ -201,7 +208,7 @@ function getBoard(socket, boardObj) {
 function getPlayers(socket, getPlayersObj) {
     console.log("Recieved: " + getPlayersObj);
     if (getPlayersObj.roomid == null) {
-        socket.emit("Match.error", { error : "Roomid is invalid"});
+        safeEmit(socket, "Match.error", { error : "Roomid is invalid"});
         return;
     }
     var match;
@@ -211,16 +218,16 @@ function getPlayers(socket, getPlayersObj) {
         match = getOpenRoom(getPlayersObj.roomid);
     }
     if (match == null) {
-        socket.emit("Match.error", { error : "Roomid is invalid"});
+        safeEmit(socket, "Match.error", { error : "Roomid is invalid"});
         return;
     }
     var players = match.getUsernames();
     if (players == null) {
-        socket.emit("Match.error", { error : "Couldn't get players"});
+        safeEmit(socket, "Match.error", { error : "Couldn't get players"});
         return;
     }
     else {
-        socket.emit("Match.players", { players : players });
+        safeEmit(socket, "Match.players", { players : players });
         if (isMatchOver(match)) {
             finishGame(match);
         }
@@ -230,11 +237,11 @@ function getPlayers(socket, getPlayersObj) {
 function leaveMatch(socket, leaveObj) {
     console.log(leaveObj);
     if (leaveObj.roomid == null) {
-        socket.emit("Match.error", { error : "Roomid is invalid"});
+        safeEmit(socket, "Match.error", { error : "Roomid is invalid"});
         return;
     }
     if (leaveObj.username == null) {
-        socket.emit("Match.error", { error : "Username is invalid"});
+        safeEmit(socket, "Match.error", { error : "Username is invalid"});
         return;
     }
     var match;
@@ -244,12 +251,47 @@ function leaveMatch(socket, leaveObj) {
         match = getOpenRoom(leaveObj.roomid);
     }
     if (match == null) {
-        socket.emit("Match.error", { error : "Roomid is invalid"});
+        safeEmit(socket, "Match.error", { error : "Roomid is invalid"});
         return;
     }
     match.leave(leaveObj.username);
     if (isMatchOver(match)) {
         finishGame(match);
+    }
+}
+//Find where the socket is connected to, leave that match
+//return nothing
+function handleDisconnect(socket) {
+    //check currentGames first
+    for (var i = 0; i < currentGameCount; i++) {
+        var key = Object.keys(currentGames)[i];
+        if (key != null) {
+            var match = currentGames[key];
+            if (match != null) {
+                //console.log("match %d was found", match.roomid);
+                var username = match.containsSocket(socket);
+                if (username != null) {
+                    console.log("%s left %d via disconnect", username, match.roomid);
+                    match.leave(username);
+                    break;
+                }
+            }
+        }
+    }
+    //check openGames
+    for (var i = 0; i < openGameCount; i++) {
+        var key = Object.keys(openGames)[i];
+        if (key != null) {
+            var match = openGames[key];
+            if (match != null) {
+                var username = match.containsSocket(socket);
+                if (username != null) {
+                    console.log("%s left %d via disconnect", username, match.roomid);
+                    match.leave(username);
+                    break;
+                }
+            }
+        }
     }
 }
 exports.init = function(io) {
@@ -280,7 +322,8 @@ exports.init = function(io) {
         });
         //When the user disconnects, perform this
         socket.on('disconnect', function() {
-            console.log("Disconnected");
+            //console.log("Disconnected");
+            handleDisconnect(socket);
         });
     });
 };
