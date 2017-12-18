ba93e1ae4c3f2cd2370729d22803dbd67c10ffe3
Ryan Everett <ryaneverett33@outlook.com>
2017-12-18 18:23:28 -0500

    getUser and addUser implemented in mysql

--- database/addUser.js
+++ database/addUser.js
@@ -1,9 +1,9 @@
-var Request = require('tedious').Request
-var TYPES = require('tedious').TYPES;
+//var Request = require('tedious').Request
+//var TYPES = require('tedious').TYPES;
 var pool = require('./pool.js');
 
 //callback(boolean) - true if successful add, false if otherwise
-function add(name, password, callback) {
+/*function add(name, password, callback) {
   pool.get(function (err, connection) {
     if (err) {
       console.error(err);
@@ -52,7 +52,56 @@ function add(name, password, callback) {
     });
     connection.execSql(request);
   });
+}*/
+function add(name, password, callback) {
+  //get a connection for multiple queries instead of using pool.query
+  pool.get(function(error, connection) {
+    if (error) {
+      console.error("An error occured adding user to databse: %s", error);
+      callback(false);
+      return;
+    }
+    else {
+      //console.log(connection);
+      //insert and then select
+      //request = new Request("INSERT users (Name, Password, Wins, HighScore) OUTPUT INSERTED.Id VALUES (@Name, @Password, 0, 0);", function (err, rowcount) {
+      connection.query('INSERT INTO users VALUES (NULL,?,?,0,0);', [name, password], function(error2, results, fields) {
+        console.log("queried");
+        if (error2) {
+          console.error("An error occured adding user to database: %s", error2);
+          callback(false);
+          try {
+            connection.release();
+          }
+          catch (e) {}
+          return;
+        }
+        else {
+          /*
+          OkPacket {
+          fieldCount: 0,
+          affectedRows: 1,
+          insertId: 0,
+          serverStatus: 2,
+          warningCount: 0,
+          message: '',
+          protocol41: true,
+          changedRows: 0 }
+          */
+          /*if (results.affectedRows == 1) {
+            console.log("added user");
+          }*/
+          if (callback != null) {
+            callback(results.affectedRows == 1);
+          }
+          pool.release(connection);
+          return;
+        }
+      });
+    }
+  });
 }
 
-
+//DOES NOT CHECK IF USER ALREADY EXISTS
+//reason: track already handles checking before calling adding
 exports.add = add;
